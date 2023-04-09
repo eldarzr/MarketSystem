e67af053251dc2646462f748da82d2a000b04032
gabi <panarovg@post.bgu.ac.il>
2023-04-09 14:21:50 +0300

    ShopBasicTests update

--- src/test/java/AcceptanceTests/Tests/ShopBasicTests.java
+++ src/test/java/AcceptanceTests/Tests/ShopBasicTests.java
@@ -11,7 +11,10 @@ import java.util.Collection;
 
 //This test class assumes GuestBasicTests are all passed
 public class ShopBasicTests extends TestCase {
+
     MarketIntr bridge;
+
+    //In set up we register 10 users (gabi0 - gabi9) and log in to the first 5 (gabi0 - gabi4)
     public void setUp() throws Exception{
         String pass = "1234Gabi";
         super.setUp();
@@ -112,9 +115,41 @@ public class ShopBasicTests extends TestCase {
             Assert.assertTrue(true);
         }
     }
-    //successful add product by appointed owner
+    //unsuccessful add product by a non appointed user
     @Test
     public void addProductTest3(){
+        try{
+            String userName = "gabi5"; // a non-appointed user
+            String shopName = "Gabi's Goods";
+            String productName = "Soccer Ball";
+            double price = 150;
+            String desc = "Adidas soccer ball size 5";
+            bridge.addNewProduct(userName,shopName,productName,desc,price);
+            Assert.fail("shouldn't be able to add a product as a non-appointed user");
+        }catch (Exception e){
+            Assert.assertTrue(true);
+        }
+    }
+    //unsuccessful add product by a non logged in owner
+    @Test
+    public void addProductTest4(){
+        try{
+            String founderName = "gabi0";
+            String userName = "gabi5";
+            String shopName = "Gabi's Goods";
+            bridge.appointShopOwner(founderName, userName, shopName);
+            String productName = "Soccer Ball";
+            double price = 100;
+            String desc = "Adidas soccer ball size 5";
+            bridge.addNewProduct(userName, shopName, productName, desc, price);
+            Assert.fail("non-logged in owner should not be able to add product");
+        } catch (Exception e){
+            Assert.assertTrue(true);
+        }
+    }
+    //successful add product by appointed owner
+    @Test
+    public void addProductTest5(){
         try{
             String founderName = "gabi0";
             String userName = "gabi1";
@@ -129,9 +164,138 @@ public class ShopBasicTests extends TestCase {
             Assert.fail(e.getMessage());
         }
     }
+//Update product tests:
+//user is only able to update a product's name in a shop if he is founder, owner or manager there, he is logged in,
+//and the new name he is trying to change the product's name to is not taken already.
+//"Gabi's Goods" shop : founder is gabi0, logged in appointed owner is gabi1, non-logged in owner is gabi5
 
+    //unsuccessful update product's name test because the name is already used for a different product in the store
+    @Test
+    public void updateProductNameTest1(){
+        try{
+            String userName = "gabi0"; // founder of Gabi's Goods
+            String oldProductName = "Basketball"; // Was added in earlier tests
+            String newProductName = "Soccer"; // Also was added in earlier tests
+            String shopName = "Gabi's Goods";
+            bridge.updateProductName(userName,shopName,oldProductName,newProductName);
+            Assert.fail("Should not be able to update a product's name to an already existing product's name in the same shop");
+        }catch (Exception e){
+            Assert.assertTrue(true);
+        }
+    }
 
+    //unsuccessful update product's name test because the user trying to do it is not authorized to
+    @Test
+    public void updateProductNameTest2(){
+        try{
+            String userName = "gabi5"; // Non-logged in owner of Gabi's Goods
+            String oldProductName = "Soccer"; // Was added in earlier tests
+            String newProductName = "Football";
+            String shopName = "Gabi's Goods";
+            bridge.updateProductName(userName,shopName,oldProductName,newProductName);
+            Assert.fail("Should not be able to update a product's name if user is not authorized");
+        }catch (Exception e){
+            Assert.assertTrue(true);
+        }
+    }
 
+    //unsuccessful update product's name test because the user trying to do it is an owner, but isn't logged in
+    @Test
+    public void updateProductNameTest3(){
+        try{
+            String userName = "gabi5"; // Non-logged in owner of Gabi's Goods
+            String oldProductName = "Soccer"; // Was added in earlier tests
+            String newProductName = "Football";
+            String shopName = "Gabi's Goods";
+            bridge.updateProductName(userName,shopName,oldProductName,newProductName);
+            Assert.fail("Should not be able to update a product's name if user is not logged in");
+        }catch (Exception e){
+            Assert.assertTrue(true);
+        }
+    }
+
+    //unsuccessful update product's name because the product itself not exists in that shop
+    @Test
+    public void updateProductNameTest4(){
+        try{
+            String userName = "gabi1"; // Logged in owner of Gabi's Goods
+            String oldProductName = "Guitar"; // A non-existing product in Gabi's Goods
+            String newProductName = "Electric Guitar";
+            String shopName = "Gabi's Goods";
+            bridge.updateProductName(userName,shopName,oldProductName,newProductName);
+            Assert.fail("Should not be able to update a product's name if the product does not exist in the shop");
+        }catch (Exception e){
+            Assert.assertTrue(true);
+        }
+    }
+
+    //successful update product's name by founder
+    @Test
+    public void updateProductNameTest5(){
+        try{
+            String userName = "gabi0"; // Founder of Gabi's Goods
+            String oldProductName = "Soccer"; // Was added in earlier tests
+            String newProductName = "Football";
+            String shopName = "Gabi's Goods";
+            bridge.updateProductName(userName,shopName,oldProductName,newProductName);
+            Assert.assertTrue(true);
+        }catch (Exception e){
+            Assert.fail("Should be able to update a product's name as a founder");
+        }
+    }
+    //successful update product's name by appointed owner who is logged in
+    @Test
+    public void updateProductNameTest6(){
+        try{
+            String userName = "gabi1"; // appointed owner of Gabi's Goods and is logged in
+            String oldProductName = "Basketball"; //Was added in earlier tests
+            String newProductName = "Tennis ball";
+            String shopName = "Gabi's Goods";
+            bridge.updateProductName(userName,shopName,oldProductName,newProductName);
+            Assert.assertTrue(bridge.productExistsInShop(shopName,newProductName));
+        }catch (Exception e){
+            Assert.fail("Should be able to update a product's name by an appointed owner who is logged in");
+        }
+    }
+    //Update product's description tests:
+    // user is only able to update a product's description in a shop if he is founder, owner or manager there, he is logged in,
+    //"Gabi's Goods" shop : founder is "gabi0", logged in appointed owner is "gabi1", non-logged in owner is "gabi5"
+    //products in "Gabi's Goods" are currently "Football" and "Tennis ball"
+
+    //unsuccessful update product's description test because the user trying to do it is not authorized to
+    @Test
+    public void updateProductDescription1(){
+        try{
+            String userName = "gabi3"; // non authorized user
+            String shopName = "Gabi's Goods";
+            String productName = "Football"; // existing product
+            String newDescription = "Brown NFL football with white stitches";
+            bridge.updateProductDesc(userName,shopName,productName,newDescription);
+            Assert.fail("user should not be able to update product's description if they aren't authorized");
+        }catch (Exception e){
+            Assert.assertTrue(true);
+        }
+    }
+    //unsuccessful update product's description test because the user trying to do it is an owner, but isn't logged in
+    @Test
+    public void updateProductDescription2(){
+
+    }
+    //unsuccessful update product's description because the product itself not exists in that shop
+    @Test
+    public void updateProductDescription3(){
+
+    }
+    //successful update product's description by logged in founder
+    @Test
+    public void updateProductDescription4(){
+
+    }
+    //successful update product's name by appointed owner who is logged in
+    @Test
+    public void updateProductDescription5(){
+
+    }
 
 
 }
