a5529f564ac9e3b4df3710347dda5ced195ff6a3
Nivuzan <nivuza@post.bgu.ac.il>
2023-05-27 23:33:50 +0300

    finish bid

--- src/main/java/ServiceLayer/DataObjects/CartDataObj.java
+++ src/main/java/ServiceLayer/DataObjects/CartDataObj.java
@@ -9,15 +9,15 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class CartDataObj {
 
-    ConcurrentHashMap<String, ShopBagDataObj> cart = new ConcurrentHashMap<>();
+    ConcurrentHashMap<String, ShopBagDataObj> cart;
 
     public CartDataObj(ConcurrentHashMap<String, ShopBagDataObj> cart) {
         this.cart = cart;
     }
 
-    public CartDataObj(Cart cart) {
-        this.cart = new ConcurrentHashMap<>();
-        Map<String, ShopBag> shopBagMap = cart.getShopsAndProducts();
+    public CartDataObj(Cart otherCart) {
+        cart = new ConcurrentHashMap<>();
+        Map<String, ShopBag> shopBagMap = otherCart.getShopsAndProducts();
         for (String key : shopBagMap.keySet())
             this.cart.put(key, new ShopBagDataObj(shopBagMap.get(key)));
     }
